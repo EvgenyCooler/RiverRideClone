c1919ca51eba137d9ae6b386237c9bfa8cc38d8f
EvgenyCooler <evgenycooler@gmail.com>
2022-02-09 21:30:32 +0200

    [+] Model Jet and Tank

---
+++ App/Assets/Scripts/Main.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] private string levelSceneName = "LevelScene";
+    void Start()
+    {
+        SceneManager.LoadSceneAsync(levelSceneName, LoadSceneMode.Additive);        
+    }
+
+}
